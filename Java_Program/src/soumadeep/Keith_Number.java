87a3d4b587bfac111b7c81086e0c4e52a1b67ff9
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-08-14 20:13:30 +0530

    Program to check Keith Number

--- Java_Program/src/soumadeep/Keith_Number.java
+++ Java_Program/src/soumadeep/Keith_Number.java
@@ -1,7 +1,8 @@
+//A positive n digit number X is called a Keith number (or repfigit number) if it is arranged in a special number sequence generated using its digits. The special sequence has first n terms as digits of x and other terms are recursively evaluated as the sum of previous n terms. For example, 197, 19, 742, 1537, etc.
 package soumadeep;
 
 class Keith_Number {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Keith Number");
     }
 }
