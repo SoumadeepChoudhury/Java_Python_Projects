1b5a6b282e60696b3c78ad97f748e9cfccdb0c17
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-08-20 23:59:08 +0530

    Program to check Keith number.

--- Java_Program/src/soumadeep/Keith_Number.java
+++ Java_Program/src/soumadeep/Keith_Number.java
@@ -1,8 +1,40 @@
-//A positive n digit number X is called a Keith number (or repfigit number) if it is arranged in a special number sequence generated using its digits. The special sequence has first n terms as digits of x and other terms are recursively evaluated as the sum of previous n terms. For example, 197, 19, 742, 1537, etc.
+//A positive n digit number X is called a Keith number (or repfigit number) if it is arranged in a special number sequence generated using its digits. The special sequence has first n terms as digits of x and other terms are recursively evaluated as the sum of previous n terms. For example, 197, 19, 742, 1537, etc. The program stops if the value computed is greater than the input number.
+
 package soumadeep;
 
+import java.util.Scanner;
+
 class Keith_Number {
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Keith Number");
+        System.out.print("Enter a number to check for Keith or not: ");
+        int number = sc.nextInt();
+        int sum = 0;
+        int num = number;
+        int arr[] = new int[Integer.toString(number).length()];
+        int i = -1;
+        while (number != 0) {
+            i++;
+            int d = number % 10;
+            arr[arr.length - 1 - i] = d;
+            number /= 10;
+        }
+        while (sum != num) {
+            sum = 0;
+            for (int j = 0; j < arr.length; j++)
+                sum += arr[j];
+            for (int j = 0; j < arr.length; j++)
+                if (j + 1 == arr.length)
+                    arr[j] = sum;
+                else
+                    arr[j] = arr[j + 1];
+            if (sum == num)
+                System.out.println("Keith Number");
+            else if (sum > num) {
+                System.out.println("Not a Keith Number");
+                break;
+            }
+        }
     }
 }
---
+++ Python Project/src/soumadeepPython/Keith_Number.py
@@ -0,0 +1,20 @@
+'''
+A positive n digit number X is called a Keith number (or repfigit number) if it is arranged in a special number sequence generated using its digits. The special sequence has first n terms as digits of x and other terms are recursively evaluated as the sum of previous n terms. For example, 197, 19, 742, 1537, etc. The program stops if the value computed is greater than the input number. 
+'''
+
+input_Number = int(input("Enter number to check for keith number: "))
+series = list(str(input_Number))
+num = input_Number
+sum = 0
+k = 0
+while(sum != num):
+    sum = 0
+    for i in range(k, len(series)):
+        sum += int(series[i])
+    series.append(str(sum))
+    k += 1
+    if sum == num:
+        print("Keith Number")
+    elif(sum > num):
+        print("Not Keith number")
+        break
