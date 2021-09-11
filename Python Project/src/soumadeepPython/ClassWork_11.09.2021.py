c46afecb43d2af5d023be1ae5afb3a54d4bc01f2
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-09-11 07:35:47 +0530

    Puthon Programs

--- Python Project/src/soumadeepPython/ClassWork_11.09.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_11.09.2021.py
@@ -8,3 +8,15 @@ def Area(r):
 radius = int(input("Enter radius: "))
 area = Area(radius)
 print(f"The area of circle is : {area}")
+
+
+# # WAP to find the sum of first n natural number using function.
+def sum(n):
+    sum = 0
+    for i in range(1, n+1):
+        sum += i
+    return sum
+
+
+user_Input = int(input("Enter value of n: "))
+print(f"Sum of {user_Input} natural number is: {sum(user_Input)}")
