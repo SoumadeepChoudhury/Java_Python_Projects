2efb1c6bd73f47205e39f04e27bf0a3f5616976c
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-09-11 07:27:16 +0530

    Python Program

--- Python Project/src/soumadeepPython/ClassWork_11.09.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_11.09.2021.py
@@ -1,7 +1,8 @@
 # # WAP to create a function in python that calculate area of the circle and return the value. Access the radius and call the above function.
 
 def Area(r):
-    return (3.14*r*r)
+    area = 3.14*r*r
+    return area
 
 
 radius = int(input("Enter radius: "))
