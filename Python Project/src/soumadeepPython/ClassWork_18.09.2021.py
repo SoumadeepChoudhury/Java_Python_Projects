3a4c11aab188e40376b9147ba87c2a7dec407108
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-09-18 07:41:57 +0530

    Python Programs.

---
+++ Python Project/src/soumadeepPython/ClassWork_18.09.2021.py
@@ -0,0 +1,12 @@
+# # Write a program that implements a user defined function that accepts Principal Amount, Rate, Time, Number of Times the interest is compounded to calculate and displays compound interest. (Hint: CI=((P*(1+R/N))NT)
+
+def CI(P, R, T, N):
+    ci = ((P*(1+R/N)*(N*T)))
+    print(f"The value of Compound Interest is {ci}")
+
+
+P = float(input("Enter Principal: "))
+R = float(input("Enter Rate: "))
+T = float(input("Enter Time: "))
+N = float(input("Enter Number of Times the interest is componded: "))
+CI(P, R, T, N)
