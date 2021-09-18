4fec07927f7a3cf4d67033cdbd0113d851304653
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-09-18 08:17:17 +0530

    Python Programs

--- Python Project/src/soumadeepPython/ClassWork_18.09.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_18.09.2021.py
@@ -1,4 +1,8 @@
+import math  # Globally defined for whole programs set.
+
 # # Write a program that implements a user defined function that accepts Principal Amount, Rate, Time, Number of Times the interest is compounded to calculate and displays compound interest. (Hint: CI=((P*(1+R/N))NT)
+
+
 def CI(P, R, T, N):
     ci = (P*(1+R/N)**(N*T))
     print(f"The value of Compound Interest is {ci}")
@@ -23,3 +27,62 @@ def swaper(num1, num2):
 num1 = int(input("Enter number 1: "))
 num2 = int(input("Enter number : "))
 print(swaper(num1, num2))
+
+
+# # Write a program that contains user defined functions to calculate area, perimeter or surface area whichever is applicable for various shapes like square, rectangle, triangle, circle and cylinder. The user defined functions should accept the values for calculation as parameters and the calculated value should be returned. Import the module and use the appropriate functions.
+def square(side):
+    area = math.pow(side, 2)
+    perimeter = math.prod(4, side)
+    return area, perimeter
+
+
+def rectangle(length, breadth):
+    area = math.prod(length, breadth)
+    perimeter = math.prod(2, (sum(length+breadth)))
+    return area, perimeter
+
+
+def triangle(a, b, c):
+    sp = sum(a, b, c)/2
+    area = math.sqrt(math.prod(sp, (sp-a), (sp-b), (sp-c)))
+    perimeter = sum(a, b, c)
+    return area, perimeter
+
+
+def circle(radius):
+    area = math.prod(3.14, math.pow(radius, 2))
+    perimeter = math.prod(2, 3.14, radius)
+    perimeter = math.prod(2, 3.14, radius)
+    return area, perimeter
+
+
+def cylinder(radius, height):
+    area = sum(math.prod(2, 3.14, radius, height),
+               math.prod(2, 3.14, math.pow(radius, 2)))
+    perimeter = sum(math.pord(4, 3.14, radius), math.prod(2, height))
+    return area, perimeter
+
+
+side_square = float(input("Enter side of square: "))
+print(f"Data from square: {square(side_square)}")
+
+length_rectangle = float(input("Enter length of rectangle: "))
+breadth_rectangle = float(input("Enter breadth of rectangle: "))
+print(
+    f"The Data for rectangle: {rectangle(length_rectangle,breadth_rectangle)}")
+
+
+side1_Triangle = float(input("Enter side1 of triangle: "))
+side2_Triangle = float(input("Enter side2 of triangle: "))
+side3_Triangle = float(input("Enter side3 of triangle: "))
+print(
+    f"The Data from Triangle :{triangle(side1_Triangle,side2_Triangle,side3_Triangle)}")
+
+
+radius_circle = float(input("Enter radius of circle: "))
+print(f"The Data from circle :{circle(radius_circle)}")
+
+
+radius_cylinder = float(input("Enter radius  of Cylinder: "))
+height_cylinder = float(input("Enter height of Cylinder: "))
+print(f"The Data from Triangle :{cylinder(radius_cylinder,height_cylinder)}")
