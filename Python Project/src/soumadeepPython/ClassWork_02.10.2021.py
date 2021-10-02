1c53ce464304aed721cb07da15a5db204e227448
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-10-02 07:29:46 +0530

    Python Practise Programs

--- Python Project/src/soumadeepPython/ClassWork_02.10.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_02.10.2021.py
@@ -1,15 +1,18 @@
-# # Create a function to convert fahrenheit to celcius & Vice versa.
+# # Create a function to convert Fahrenheit to Celsius & Vice versa.
 
 def temperature(value, unit):
     conv = 0
     if(unit == 'F'):
         conv = (value-32)*5/9
+        unit = 'C'
     else:
         conv = (value*9/5)+32
-    print(f"The required temp is {conv}")
+        unit = 'F'
+    print(f"The required temp is {conv} {unit}")
 
 
-temp = input("Enter temperature in F/C scale: ").strip()
+temp = input(
+    "Enter temperature in F/C scale (eg: 98 F or 38 C etc): ").upper().strip()
 unit = temp[-1]
-value = int(temp[0:len(temp)-2])
+value = int(temp[0:len(temp)-1].strip())
 temperature(value, unit)
