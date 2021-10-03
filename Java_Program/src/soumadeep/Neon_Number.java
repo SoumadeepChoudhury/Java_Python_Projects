1647afb73379b4d5f060ac0c15cc5ac54f0e8fe4
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-10-03 23:36:09 +0530

    Neon Num

--- Java_Program/src/soumadeep/Neon_Number.java
+++ Java_Program/src/soumadeep/Neon_Number.java
@@ -15,7 +15,7 @@ public class Neon_Number {
         while (squared > 0) {
             int d = squared % 10;
             sum += d;
-            squared /= 10;
+            squared /= 100;
         }
         if (sum == user_Input) {
             System.out.println("It is a Neon Number.");
