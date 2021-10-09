aefbd6cdc25852dba976fd8e8420b577e2c7d94c
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-10-09 07:55:03 +0530

    Python practise programs.

--- Python Project/src/soumadeepPython/ClassWork_09.10.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_09.10.2021.py
@@ -10,3 +10,18 @@ for i in numbers:
     if(i % 2 != 0):
         odd += (i,)
 print(odd)
+
+
+# # Write a program to read email IDs of n number of students and store them in a tuple. Create two new tuples, one to store only the usernames from the email IDs and second to store domain names from the email ids. Print all three tuples at the end of the program.
+
+
+# # Write a program to input names of n students and store them in a tuple. Also, input a name from the user and find if this student is present in the tuple or not.
+names = tuple()
+n = int(input("How many names would you like to enter? "))
+for _ in range(n):
+    names += (input("Enter name: "),)
+name_Check = input("Enter name to check within list: ")
+if(name_Check in names):
+    print("Name found in tuple")
+else:
+    print("Name not found in tuple")
