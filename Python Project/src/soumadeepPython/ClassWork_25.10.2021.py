84bd58b6776608bb2067ffa27412057760d08ae8
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-10-30 07:15:46 +0530

    Python Practise programs

--- Python Project/src/soumadeepPython/ClassWork_25.10.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_25.10.2021.py
@@ -1,12 +1,12 @@
-# # # Print the series 1,-4,7,-10....-40
-# k = 1
-# for i in range(1, 41, 3):
-#     print(i*k, end=" ")
-#     k *= -1
+# # Print the series 1,-4,7,-10....-40
+k = 1
+for i in range(1, 41, 3):
+    print(i*k, end=" ")
+    k *= -1
 
 
 # # WAP accept a sentence and display a word formed by each first letter of the sentence.
-userInp = input("Enter a sentence: ")
+userInp = input("Enter a sentence: ").strip()
 newWord = userInp[0]
 for i in range(0, len(userInp)):
     if userInp[i] == " ":
