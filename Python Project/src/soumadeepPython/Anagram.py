dfb729bdb6e04b0b1f2580b2733d18828b5f3b60
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-11-12 12:53:43 +0530

    Updated Anagram

--- Python Project/src/soumadeepPython/Anagram.py
+++ Python Project/src/soumadeepPython/Anagram.py
@@ -4,6 +4,6 @@ s = input("Enter A String: ").lower()
 sn = input("Enter another String: ").lower()
 c1, c2 = 0, 0
 if(sorted(s) == sorted(sn)):
-    print("Anagrams")
+    print("Anagram")
 else:
     print("Not Anagram")
