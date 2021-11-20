a3e693a5830b8196916958e2a7a98455c8d7099c
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-11-20 06:01:27 +0530

    HomeWork of Dictionary.

--- Python Project/src/soumadeepPython/ClassWork_13.11.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_13.11.2021.py
@@ -55,3 +55,67 @@ for i in monthDays:
         print(i)
 sort = sorted([(i, j) for j, i in monthDays.items()])
 print([(i, j) for i, j in sort])
+
+
+# # Given the dictionary x = {'k1':'v1','k2':'v2','k3':'v3'}, create a dictionary with the opposite mapping that is written writer program to create the dictionary as: inverted_x = {'v1':'k1','v2':'k2','v3':'k3'}
+x = {'k1': 'v1', 'k2': 'v2', 'k3': 'v3'}
+keys = list(x.keys())
+values = list(x.values())
+inverted_x = {}
+for i in range(len(keys)):
+    inverted_x[values[i]] = keys[i]
+print(inverted_x)
+
+
+# # Given 2 dictionaries saying D1 and D2. Write a program that lists the overlapping keys of the two dictionaries, that is, if a key of D1 is also a key of D2, then list it.
+D1 = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+D2 = {
+    "brand": "Ford",
+    "electric": False,
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
+D1Keys = list(D1.keys())
+D2Keys = list(D2.keys())
+x = D2Keys if len(D2Keys) < len(D1Keys) else D1Keys
+y = D1Keys if(x == D2Keys) else D2Keys
+for i in (x):
+    if(i in y):
+        print(i)
+
+
+# # Write a program that checks if two same values in a dictionary have different keys. That is, for dictionary D1 = { ‘a’ : 10, ‘b': 20, ‘c’ : 10}, the program should print “2 keys have same values” and for dictionary D2 = { ‘a’ : 10, ‘b’ : 20, ‘c’ : 30} , the program should print “No keys have same values”.
+D1 = {'a': 10, 'b': 20, 'c': 10}
+values = list(D1.values())
+x = "000"
+for i in values:
+    if x != i and values.count(i) > 1:
+        print(f"{values.count(i)} keys have same values")
+    x = i
+
+
+# # WAP to check if a dictionary is contained in another dictionary.
+# # d1 = {1:11,2:12}
+# # d2 = {1:11,2:12,3:13,4:15} ........... Therefore d1 is contained in d2.
+d1 = {1: 11, 2: 12}
+d2 = {1: 11, 2: 12, 3: 13, 4: 15}
+d1Pairs = list(d1.items())
+d2Pairs = list(d2.items())
+for i in d1Pairs:
+    if i in d2Pairs:
+        print("First Dictionary is contained in another Dictionary")
+        break
+
+
+# # A dictionary has values in the form of list of numbers. Write a program to create a new dictionary D2 having same keys as D1 but values as the sum of the list elements.
+D1 = {'A': [1, 2, 3], 'B': [4, 5, 6]}
+values = list(D1.values())
+D2 = {}
+keys = list(D1.keys())
+for i in range(len(keys)):
+    D2[keys[i]] = sum(values[i])
+print(D2)
