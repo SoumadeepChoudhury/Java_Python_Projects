41210fabf042f002e761a2cc9948851af644782a
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-11-21 16:57:55 +0530

    Updated Homework 20_11_2021

--- Python Project/src/soumadeepPython/ClassWork_13.11.2021.py
+++ Python Project/src/soumadeepPython/ClassWork_13.11.2021.py
@@ -9,8 +9,18 @@ print(employees)
 
 
 # # WAP To convert a number entered by the user into its corresponding number in words. For example, if the input is 876 then the output should be 'Eight Seven Six'.
-Num_Words = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four",
-             5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"}
+Num_Words = {
+    0: "Zero",
+    1: "One",
+    2: "Two",
+    3: "Three",
+    4: "Four",
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
+}
 l = []
 user_Inp = int(input("Enter whole Number: "))
 while(user_Inp > 0):
