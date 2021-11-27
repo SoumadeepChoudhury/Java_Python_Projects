02d321ef8bc8739cd39f6bfbeea7fcf047f1bf04
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-11-27 14:04:12 +0530

    ClassWork_27.11.2021

---
+++ Python Project/src/soumadeepPython/ClassWork_27.11.2021.py
@@ -0,0 +1,10 @@
+# # WAP to accept a number and display all the factors which are prime ( prime factors)
+user_Inp = int(input("Enter number: "))
+for i in range(1, user_Inp+1):
+    c = 0
+    if(user_Inp % i == 0):
+        for j in range(1, i+1):
+            if(i % j == 0):
+                c += 1
+        if(c <= 2):
+            print(i)
