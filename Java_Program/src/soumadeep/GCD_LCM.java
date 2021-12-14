6954eff369df686dcdec8f4453abe8be6b712b53
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2021-12-15 01:29:16 +0530

    updated gcd and lcm program

--- Java_Program/src/soumadeep/GCD_LCM.java
+++ Java_Program/src/soumadeep/GCD_LCM.java
@@ -12,14 +12,15 @@ class GCD_LCM {
         System.out.print("Enter second number ");
         int n2 = sc.nextInt();
         int b = n1;
-        int gcd = 0, lcd = 0;
+        int gcd = 0;
+        double lcd = 0.0;
         if (n1 > n2)
             b = n2;
         for (int i = 1; i <= b; i++) {
             if (n1 % i == 0 && n2 % i == 0)
                 gcd = i;
         }
-        lcd = (n1 * n2) / gcd;
+        lcd = (1.0 * n1 * n2) / gcd;
         System.out.println("GCD: " + gcd);
         System.out.println("LCM: " + lcd);
     }
