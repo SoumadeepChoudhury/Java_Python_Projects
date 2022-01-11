2419a64d7c2bde1186b3e60341e6eebeee3c6af0
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-01-11 23:39:58 +0530

    ProgramOfTheYear..............

--- Python Project/src/soumadeepPython/ProgramOfTheYear.py
+++ Python Project/src/soumadeepPython/ProgramOfTheYear.py
@@ -1,8 +1,8 @@
 '''You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, age and height are numbers. The tuples are input by console. The sort criteria is:
 1: Sort based on name
 2: Then sort based on age
-3: Then sort by score.
-The priority is that name > age > score.
+3: Then sort by height.
+The priority is that name > age > height.
 If the following tuples are given as input to the program:
 Tom, 19, 80
 John, 20, 90
