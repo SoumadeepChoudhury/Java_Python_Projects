fefca34415ccf3b203f49719611db7945989e28d
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-01-22 19:28:25 +0530

    Updated Perfect Square Number

--- Python Project/src/soumadeepPython/Perfect Square.py
+++ Python Project/src/soumadeepPython/Perfect Square.py
@@ -1,8 +1,7 @@
 # WAP accept 10 elements in a list and display the perfect square elements in a list.
-from math import pow
 li = []
-for _ in range(10):
+for _ in range(3):
     li.append(int(input("Enter value: ")))
 for i in range(len(li)):
-    if(pow(li[i], 0.5) == int(pow(li[i], 0.5))):
+    if((li[i]**0.5) == int(li[i]**0.5)):
         print(li[i])
