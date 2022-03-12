87838d92ef621c8354fe2acd23e2bf7414b94ef6
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-12 07:39:19 +0530

    Updated Classwork...

--- Python Project/src/soumadeepPython/bb.py
+++ Python Project/src/soumadeepPython/bb.py
@@ -4,3 +4,11 @@ s = ''
 for i in range(len(userInp)):
     s += userInp[i][0].upper()+userInp[i][1:]+" "
 print(s)
+
+
+# WAP accept the setence and reverse each word of the sentence and display it.
+userInp = input("Enter sentence: ").split(" ")
+s = ''
+for i in range(len(userInp)):
+    s += userInp[i][::-1]+" "
+print(s)
