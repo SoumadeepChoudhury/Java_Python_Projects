1fc41bfac2b01889279864c5b1d3d34381dec35e
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-12 07:41:08 +0530

    Updated

--- Python Project/src/soumadeepPython/bb.py
+++ Python Project/src/soumadeepPython/bb.py
@@ -8,7 +8,6 @@ print(s)
 
 # WAP accept the setence and reverse each word of the sentence and display it.
 userInp = input("Enter sentence: ").split(" ")
-s = ''
 for i in range(len(userInp)):
-    s += userInp[i][::-1]+" "
-print(s)
+    userInp[i] = userInp[i][::-1]
+print(' '.join(map(str, userInp)))
