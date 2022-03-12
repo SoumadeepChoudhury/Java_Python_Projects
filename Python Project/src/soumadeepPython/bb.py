4d6926a629f9dbe2990775a78969e87ed59765bf
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-12 07:29:30 +0530

    updated

--- Python Project/src/soumadeepPython/bb.py
+++ Python Project/src/soumadeepPython/bb.py
@@ -1,5 +1,5 @@
 # WAP accept sentence tell the first letter of each word to capital and display the new sentence.
-userInp = input("Enter snetence: ").split(" ")
+userInp = input("Enter sentence: ").split(" ")
 s = ''
 for i in range(len(userInp)):
     s += userInp[i][0].upper()+userInp[i][1:]+" "
