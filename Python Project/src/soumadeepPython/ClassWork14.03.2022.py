9b596c227e80eda57aa82f32ca7a5da926984ec4
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-14 20:18:10 +0530

    Updated

--- Python Project/src/soumadeepPython/ClassWork14.03.2022.py
+++ Python Project/src/soumadeepPython/ClassWork14.03.2022.py
@@ -1,19 +1,19 @@
-# # WAP accept 15 words in a list display only palindrome word and special word.
-# UserList = eval(input("Enter 15 words in a list: "))
-# palindrome = []
-# special = []
-# for i in UserList:
-#     if i == i[::-1]:
-#         palindrome.append(i)
-#     elif i[0] == i[-1]:
-#         special.append(i)
-# print(i for i in palindrome)
-# print(i for i in special)
+# WAP accept 15 words in a list display only palindrome word and special word.
+UserList = eval(input("Enter 15 words in a list: "))
+palindrome = []
+special = []
+for i in UserList:
+    if i == i[::-1]:
+        palindrome.append(i)
+    elif i[0] == i[-1]:
+        special.append(i)
+print(i for i in palindrome)
+print(i for i in special)
 
-# # WAP accept a string in uppercase.Convert all letters of the word other than the first letter to lowercase. Display the new string...
-# userInp = input("Enter a String in uppercase: ").upper().split(" ")
-# for i in userInp:
-#     print(i[0]+i[1:].lower(), end=" ")
+# WAP accept a string in uppercase.Convert all letters of the word other than the first letter to lowercase. Display the new string...
+userInp = input("Enter a String in uppercase: ").upper().split(" ")
+for i in userInp:
+    print(i[0]+i[1:].lower(), end=" ")
 
 
 # WAP input a string. Print the new string after converting every alternate letter to uppercase and next immidiate letter in lower case. Special character remains same
