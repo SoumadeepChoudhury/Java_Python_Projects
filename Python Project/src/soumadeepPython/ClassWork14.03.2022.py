1c79b35b5761bd1f7278546badb11973f39b548e
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-14 19:31:04 +0530

    Updated

--- Python Project/src/soumadeepPython/ClassWork14.03.2022.py
+++ Python Project/src/soumadeepPython/ClassWork14.03.2022.py
@@ -1,7 +1,11 @@
 # WAP accept 15 words in a list display only palindrome word and special word.
 UserList = eval(input("Enter 15 words in a list: "))
+palindrome = []
+special = []
 for i in UserList:
     if i == i[::-1]:
-        print(f"{i} is a palindrome word")
+        palindrome.append(i)
     elif i[0] == i[-1]:
-        print(f"{i} is a special word")
+        special.append(i)
+print(i for i in palindrome)
+print(i for i in special)
