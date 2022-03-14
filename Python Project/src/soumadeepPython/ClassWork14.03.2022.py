0cf0061479ed90f978c803d907529f1aa257c0a0
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-14 19:24:49 +0530

    Added

---
+++ Python Project/src/soumadeepPython/ClassWork14.03.2022.py
@@ -0,0 +1,7 @@
+# WAP accept 15 words in a list display only palindrome word and special word.
+UserList = eval(input("Enter 15 words in a list: "))
+for i in UserList:
+    if i == i[::-1]:
+        print(f"{i} is a palindrome word")
+    if i[0] == i[-1]:
+        print(f"{i} is a special word")
