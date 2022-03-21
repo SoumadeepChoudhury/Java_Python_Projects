ed0eef362c41cd8a14a9fa968565062e40f2348f
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-21 19:49:39 +0530

    upadted

--- Python Project/src/soumadeepPython/ClassWork_21.03.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_21.03.2022.py
@@ -19,3 +19,14 @@ for i in range(len(userInp)-1):
     if userInp[i] == userInp[i+1]:
         count += 1
 print(count)
+
+
+# WAP accept a string and display maximum occurence character in the string.
+userInp = input("Enter String: ")
+count = 0
+char = ''
+for i in userInp:
+    if count < userInp.count(i):
+        count = userInp.count(i)
+        char = i
+print(char)
