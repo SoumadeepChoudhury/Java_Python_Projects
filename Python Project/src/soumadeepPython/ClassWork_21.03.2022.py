8ba3b3e917e9f029efdf49562d54a3626f0bdc25
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-21 19:38:55 +0530

    updated

--- Python Project/src/soumadeepPython/ClassWork_21.03.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_21.03.2022.py
@@ -10,3 +10,12 @@ The string is to be replaced by user Input string.
 userInp = input("Enter String: ")
 for i in range(len(userInp)):
     print(" ".join(userInp[i:]))
+
+
+# WAP accept a string. Convert the string to uppercase. Count and output the number of double letter sequence that exists in the string.
+userInp = input("Enter sentence: ")
+count = 0
+for i in range(len(userInp)-1):
+    if userInp[i] == userInp[i+1]:
+        count += 1
+print(count)
