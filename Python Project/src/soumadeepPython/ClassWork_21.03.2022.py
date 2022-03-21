59c5251bf393aa3fe49c8a46e99797acaa8f48e1
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-03-21 20:25:34 +0530

    updated

--- Python Project/src/soumadeepPython/ClassWork_21.03.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_21.03.2022.py
@@ -35,10 +35,15 @@ print(char)
 # WAP accept a sentence in uppercase and change the each vowel with next vowel.
 userInp = input("Enter sentence: ").upper()
 vowel = ['A', 'E', 'I', 'O', 'U']
-for i in range(len(userInp)):
-    if userInp[i] in vowel:
-        userInp = userInp.replace(userInp[i], vowel[vowel.index(
-            userInp[i])+1 if userInp[i] != 'U' else 0])
+# for i in range(len(userInp)):
+#     if userInp[i] in vowel:
+#         userInp = userInp.replace(userInp[i], vowel[vowel.index(
+#             userInp[i])+1 if userInp[i] != 'U' else 0])
+userInp = userInp.replace('A', 'E')
+userInp = userInp.replace('E', 'I')
+userInp = userInp.replace('I', 'O')
+userInp = userInp.replace('O', 'U')
+userInp = userInp.replace('U', 'A')
 print(userInp)
 
 # WAP accept ten names and print those names which starts with given letter by user.
