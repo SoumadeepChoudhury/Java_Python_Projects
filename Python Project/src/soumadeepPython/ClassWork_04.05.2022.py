5d9b6b06aa464a6bc390322d101b1d3099cdbd2c
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-05-04 07:33:41 +0530

    Updated

---
+++ Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
@@ -0,0 +1,14 @@
+# WAP that inputs a main string and then creates an encrypted string by embedding a short symbol based after each character.The program should also be able to produce the decrypt string from the encrypted string.
+def encrypt(string: str) -> str:
+    encStr = '#.$%'.join(string)
+    return encStr
+
+
+def decrypt(string: str) -> str:
+    decStr = ''.join(string.split("#.$%"))
+    return decStr
+
+
+userInput = input("Enter user Input to encrypt: ")
+print("After Encrypting: ", encrypt(userInput))
+print("After Decrypting: ", decrypt(encrypt(userInput)))
