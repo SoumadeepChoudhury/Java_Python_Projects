76272e46a2796ec0811215291c403c1dbf490286
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-05-04 07:39:56 +0530

    updated

--- Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
@@ -1,6 +1,11 @@
 # WAP that inputs a main string and then creates an encrypted string by embedding a short symbol based after each character.The program should also be able to produce the decrypt string from the encrypted string.
 def encrypt(string: str) -> str:
-    encStr = '#.$%'.join(string)
+    encStr = ''
+    for i in string:
+        if i is ' ':
+            encStr += i
+            continue
+        encStr += i+"#.$%"
     return encStr
 
 
