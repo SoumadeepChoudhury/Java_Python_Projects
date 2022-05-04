2e32b4f341a0084f2ba25b4482812e12f6052d1e
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-05-04 08:15:37 +0530

    updated with random encrytion key

--- Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
@@ -1,4 +1,13 @@
 # WAP that inputs a main string and then creates an encrypted string by embedding a short symbol based after each character.The program should also be able to produce the decrypt string from the encrypted string.
+from random import randint
+ENCKEYLIST = "!@#$%^&*()_+="
+rangeStart, rangeEnd = 0, 0
+while (rangeEnd <= rangeStart):
+    rangeStart = randint(0, 12)
+    rangeEnd = randint(0, 12)
+encKey = ENCKEYLIST[rangeStart:rangeEnd]
+
+
 def encrypt(string: str) -> str:
     encStr = ''
     temp = 0
@@ -7,14 +16,14 @@ def encrypt(string: str) -> str:
             encStr += string[i]
             continue
         if i != 0:
-            encStr += string[temp:i]+"#.$%"
+            encStr += string[temp:i]+encKey
         temp = i
-    encStr += string[temp:]+"#.$%"
+    encStr += string[temp:]+encKey
     return encStr
 
 
 def decrypt(string: str) -> str:
-    decStr = ''.join(string.split("#.$%"))
+    decStr = ''.join(string.split(encKey))
     return decStr
 
 
