7f77c2475735d4b6941eac0d4db65a7c368f249c
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-05-06 00:43:43 +0530

    updated added HW

--- Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_04.05.2022.py
@@ -30,3 +30,35 @@ def decrypt(string: str) -> str:
 userInput = input("Enter user Input to encrypt: ")
 print("After Encrypting: ", encrypt(userInput))
 print("After Decrypting: ", decrypt(encrypt(userInput)))
+
+
+# Q. Write a Python program having following functions:
+# (i) A function with the following signature:
+#       remove_letter (sentence, letter)
+# This function should take a string and a letter (as a single-character string) as arguments returning a copy of that string with every instance of the indicated letter removed
+# For example remove_letter ("Hello there!", "e") should return the string "Hllo thr!".
+
+# (ii) Write a function to do the following :
+# Try implementing the capwords () functionality using other functions, i.e , split(), capitalize () and join( ). Compare the result with the capwords() function's result.
+def remove_letter(sentence: str, letter: str) -> str:
+    sentence = sentence.replace(letter, '')
+    return sentence
+
+
+def capwords(sentence: str) -> str:
+    senList = sentence.split()
+    for i in range(len(senList)):
+        word = ''
+        for j in senList[i]:
+            word += j.capitalize()
+        senList[i] = word
+    sentence = ' '.join(senList)
+    return sentence
+
+
+print("After removing desizered letter: ", remove_letter(
+    input("Enter sentence: "), input("Enter letter to be removed: ")))
+print("Use of Capwords function: ", capwords(
+    input("Enter sentence to CAPITALISE: ")))
+print("No Use of Capwords function: ", input(
+    "Enter sentence to CAPITALISE: ").upper())
