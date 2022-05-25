21eaabedae7ce241dbfe2794b716639f45e9128e
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-05-25 07:47:10 +0530

    added Q1

--- Python Project/src/soumadeepPython/Classwork_25.05.2022.py
+++ Python Project/src/soumadeepPython/Classwork_25.05.2022.py
@@ -1,32 +1,34 @@
-# # # Sumita Arora Class 12
+# Sumita Arora Class 12
 
-# # # Q6 Pg: 253
-# # fileInp = open("Article.txt", "a+")
-# # fileInp.write("Hello World")
-# # text = fileInp.read()
-# # count = 0
-# # for i in text:
-# #     if i == i.upper():
-# #         count += 1
-# # print("Number of upper case characters: ", count)
-# # fileInp.close()
+# Q6 Pg: 253
+fileInp = open("Article.txt", "a+")
+fileInp.write("Hello World")
+text = fileInp.read()
+count = 0
+for i in text:
+    if i == i.upper():
+        count += 1
+print("Number of upper case characters: ", count)
+fileInp.close()
 
-# # # Q9 Pg: 253
-# # def DISPLAYWORDS():
-# #     file = open("STORY.txt", "r")
-# #     text = file.readlines()
-# #     for i in text:
-# #         i.replace("\n", "")
-# #         for j in i:
-# #             if len(j) < 4:
-# #                 print(j)
+# Q9 Pg: 253
 
 
-# # Q1 Pg:253
-# file=open("text.txt","r")
-# text=file.read()
-# text.replace("  "," ")
-# file.close()
-# file=open("CopyText.txt","w+")
-# file.write(text)
-# file.close()
+def DISPLAYWORDS():
+    file = open("STORY.txt", "r")
+    text = file.readlines()
+    for i in text:
+        i.replace("\n", "")
+        for j in i:
+            if len(j) < 4:
+                print(j)
+
+
+# Q1 Pg:253
+file = open("text.txt", "r")
+text = file.read()
+text.replace("  ", " ")
+file.close()
+file = open("CopyText.txt", "w+")
+file.write(text)
+file.close()
