322339210b01bdf495b107b4e56d7b5a726c72b7
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-06-22 08:11:08 +0530

    updated

--- Python Project/src/soumadeepPython/ClassWork_22.06.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_22.06.2022.py
@@ -10,40 +10,46 @@
 #         if line[0].isupper():
 #             print(line)
 
-'''Write a class CONTAINER in Python with the following specifications : Instance attributes
-- Radius,Height # Radius and Height of Container
-- Type # Type of Container
-- Volume # Volume of Container
-Methods
-- CalVolume() # To calculate volume
-# as per the Type of container
-# With the formula as given below :
-Type
-Formula to Calculate Volume
-1
-3.14 * Radius * Height
-3 3.14 * Radius * Height/3
-- GetValue() # To allow user to enter values of
-# Radius, Height and Type.
-# Also, this method should call
-# CalVolume() to calculate Volume
-- ShowContainer() # To display Radius, Height, Type
-# Volume of the Container'''
-class CONTAINER:
-    def __init__(self) -> None:
-        self.RADIUS,self.HEIGHT,self.TYPE,self.VOLUME=0,0,0,0
-    def CallVolume(self):
-        if self.TYPE==1:
-            self.VOLUME=3.14*self.RADIUS*self.HEIGHT
-        elif self.TYPE==2:
-            self.VOLUME=3.14*self.RADIUS*(self.HEIGHT/3)
-        self.ShowContainer()
-    def GetValue(self):
-        self.RADIUS=int(input("Enter radius: "))
-        self.HEIGHT=int(input("Enter Height: "))
-        self.TYPE=int(input("Enter Type: "))
-        self.CallVolume()
-    def ShowContainer(self):
-        print(self.TYPE,self.RADIUS,self.HEIGHT,self.VOLUME)
-Object = CONTAINER()
-Object.GetValue()
+# '''Write a class CONTAINER in Python with the following specifications : Instance attributes
+# - Radius,Height # Radius and Height of Container
+# - Type # Type of Container
+# - Volume # Volume of Container
+# Methods
+# - CalVolume() # To calculate volume
+# # as per the Type of container
+# # With the formula as given below :
+# Type
+# Formula to Calculate Volume
+# 1
+# 3.14 * Radius * Height
+# 3 3.14 * Radius * Height/3
+# - GetValue() # To allow user to enter values of
+# # Radius, Height and Type.
+# # Also, this method should call
+# # CalVolume() to calculate Volume
+# - ShowContainer() # To display Radius, Height, Type
+# # Volume of the Container'''
+# class CONTAINER:
+#     def __init__(self) -> None:
+#         self.RADIUS,self.HEIGHT,self.TYPE,self.VOLUME=0,0,0,0
+#     def CallVolume(self):
+#         if self.TYPE==1:
+#             self.VOLUME=3.14*self.RADIUS*self.HEIGHT
+#         elif self.TYPE==2:
+#             self.VOLUME=3.14*self.RADIUS*(self.HEIGHT/3)
+#         self.ShowContainer()
+#     def GetValue(self):
+#         self.RADIUS=int(input("Enter radius: "))
+#         self.HEIGHT=int(input("Enter Height: "))
+#         self.TYPE=int(input("Enter Type: "))
+#         self.CallVolume()
+#     def ShowContainer(self):
+#         print(self.TYPE,self.RADIUS,self.HEIGHT,self.VOLUME)
+# Object = CONTAINER()
+# Object.GetValue()
+
+# Write a method in Python to read lines from a text file INDIA.TXT, to find and display the occurrence of the word ‘‘India’’.
+def reader():
+    file=open("tt.txt").read()
+    print(file.count("India"))
+reader()
