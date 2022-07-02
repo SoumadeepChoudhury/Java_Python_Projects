af8f34d4c0271e019b42126e788f69515188c481
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-07-02 08:25:41 +0530

    Updated

--- Python Project/src/soumadeepPython/ClassWork_02.07.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_02.07.2022.py
@@ -1,18 +1,18 @@
-# # Program in python to type text in a file till ~ is pressed as rightmost character.
-# file=open("file.txt","a")
-# while True:
-#     fileinput=input("Enter text: ")
-#     if fileinput.endswith('~'):
-#         file.write(fileinput)
-#         break
-#     else:
-#         file.write(fileinput)
-# file.close()
+# Program in python to type text in a file till ~ is pressed as rightmost character.
+file=open("file.txt","a")
+while True:
+    fileinput=input("Enter text: ")
+    if fileinput.endswith('~'):
+        file.write(fileinput)
+        break
+    else:
+        file.write(fileinput)
+file.close()
 
-# # WAP to read first 5 lines from the text file.
-# file=open("file.txt")
-# for i in range(5):
-#     print(file.readline())
+# WAP to read first 5 lines from the text file.
+file=open("file.txt")
+for i in range(5):
+    print(file.readline())
 
 # WAP to combine each line from first file with the corresponding line of second file.
 file1=open("tt.txt")
