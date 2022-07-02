6090c829ecde7a9e772374de2a9debe187733cea
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-07-02 08:00:20 +0530

    Updated

--- Python Project/src/soumadeepPython/ClassWork_02.07.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_02.07.2022.py
@@ -1,9 +1,15 @@
-# Program in python to type text in a file till ~ is pressed as rightmost character.
-file=open("file.txt","a")
-while True:
-    fileinput=input("Enter text: ")
-    if fileinput.endswith('~'):
-        file.write(fileinput)
-        break
-    else:
-        file.write(fileinput)
+# # Program in python to type text in a file till ~ is pressed as rightmost character.
+# file=open("file.txt","a")
+# while True:
+#     fileinput=input("Enter text: ")
+#     if fileinput.endswith('~'):
+#         file.write(fileinput)
+#         break
+#     else:
+#         file.write(fileinput)
+# file.close()
+
+# WAP to read first 5 lines from the text file.
+file=open("tt.txt")
+for i in range(5):
+    print(file.readline())
