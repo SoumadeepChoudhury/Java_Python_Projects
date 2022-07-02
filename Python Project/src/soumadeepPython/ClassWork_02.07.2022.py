0bc642a7dad7728df28fa5329a1615d14966bbdf
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-07-02 07:49:38 +0530

    Uploaded

---
+++ Python Project/src/soumadeepPython/ClassWork_02.07.2022.py
@@ -0,0 +1,9 @@
+# Program in python to type text in a file till ~ is pressed as rightmost character.
+file=open("file.txt","a")
+while True:
+    fileinput=input("Enter text: ")
+    if fileinput.endswith('~'):
+        file.write(fileinput[:len(fileinput)-2])
+        break
+    else:
+        file.write(fileinput)
