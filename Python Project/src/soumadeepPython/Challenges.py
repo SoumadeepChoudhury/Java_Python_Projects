278ae1fc21ff8cdc2c91c31537f6c0c9bae7fb06
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-07-20 03:03:16 +0530

    Updated added Challenge #2 DONE

--- Python Project/src/soumadeepPython/Challenges.py
+++ Python Project/src/soumadeepPython/Challenges.py
@@ -16,3 +16,32 @@ def pascal()->str:
             pasno=pasno*11
     file.write(line)
     return (line+'\nSuccessfully Added in file')
+
+
+#2) WAP that will generate all the numbers of 4 digit... from 0 to 9 ........all the numbers will get stored in the "COLLECTOR" db table with sl no ..ND when all the numbers get stored in the db then print in the shell.  "PROGRAMS TERMINATES AFTER N SECONDS"  with the total numbers that get stored in the database table..
+import mysql.connector
+from random import shuffle   #optional
+import time
+start=time.time()
+mydb = mysql.connector.connect(
+  host="hostname",
+  user="username",
+  password="password",
+  database="databasename"
+)
+mycursor = mydb.cursor()
+if mydb:
+    query="create table if not exists Chlgn2(Slno int(4) primary key AUTO_INCREMENT,Num int(4));"
+    mycursor.execute(query)
+    numList=[]
+    for i in range(1000,10000):
+        numList.append(i)
+    shuffle(numList)   #optional
+    for i in numList:
+        query=f"insert into Chlgn2(Num) values({i})"
+        mycursor.execute(query)
+        mydb.commit()
+    end=time.time()
+    print(f"PROGRAMS TERMINATES AFTER {end-start} SECONDS")
+else:
+    print("Failed to connect. Try again later")
