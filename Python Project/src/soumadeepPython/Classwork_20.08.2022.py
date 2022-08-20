eda3d5562af332cbb37ec5f52450690e86717cc5
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-08-20 07:39:58 +0530

    updated question

--- Python Project/src/soumadeepPython/Classwork_20.08.2022.py
+++ Python Project/src/soumadeepPython/Classwork_20.08.2022.py
@@ -12,6 +12,7 @@ for i in range(len(li)):
     else:
         continue
 # exchange element of 1st half with second half of list of 7.
+# Eg: [1,2,3,4,5,6,7] => [5,6,7,4,1,2,3]
 li=[1,2,3,4,5,6,7]
 print(li)
 x1,x2,x3=li[0],li[1],li[2]
