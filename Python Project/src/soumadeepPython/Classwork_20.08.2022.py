37123741f0e1bcfe3f97676f64d6d39bd1fc6765
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-08-20 07:28:45 +0530

    added

--- Python Project/src/soumadeepPython/Classwork_20.08.2022.py
+++ Python Project/src/soumadeepPython/Classwork_20.08.2022.py
@@ -3,3 +3,11 @@ li=[[1,2],[2,3],[3,4]]
 for i in li:
     for j in i:
         print(j)
+
+# display skipping element of 1D list of 7 element.
+li=[1,2,3,4,5,6,7]
+for i in range(len(li)):
+    if i%2==0:
+        print(li[i])
+    else:
+        continue
