ea6ca8a20cdb6eeda7f61db65a53d49a383262f6
SoumadeepChoudhury <chanchalchoudhury.2018@gmail.com>
2022-11-02 08:35:24 +0530

    Updated

--- Python Project/src/soumadeepPython/ClassWork_02.11.2022.py
+++ Python Project/src/soumadeepPython/ClassWork_02.11.2022.py
@@ -8,4 +8,9 @@ def disarium(number:int)->int:
 
 
 
-## Write a program for Triangular Number...
+# Write a program for Triangular Number...
+def triangularNum(value:int,num:int=1,prevValue:int=0)->int:
+    if prevValue==value:
+        return value
+    prevValue=prevValue+num
+    return triangularNum(value,num+1,prevValue)
